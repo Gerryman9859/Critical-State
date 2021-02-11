117f8da4f49cccb8d24f15ab6af0c5b61d4aee23
luisVDLc <60795327+luisVDLc@users.noreply.github.com>
2021-02-11 16:54:42 +0100

    Tests IA y optimización recogida de objetos

---
+++ Source/Critical_State/Test_DialogC.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Test_DialogC.h"
+
+Test_DialogC::Test_DialogC()
+{
+}
+
+Test_DialogC::~Test_DialogC()
+{
+}
---
+++ Source/Critical_State/Test_DialogC.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class CRITICAL_STATE_API Test_DialogC
+{
+public:
+	Test_DialogC();
+	~Test_DialogC();
+};
